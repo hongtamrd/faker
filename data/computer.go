df656a822261536ca02fe95f9f462efaf9113d4b
hongtamrd <hongtamrd@gmail.com>
2022-03-06 09:27:55 +0700

    Update computer.go

--- data/computer.go
+++ data/computer.go
@@ -4,6 +4,6 @@ package data
 var Computer = map[string][]string{
 	"linux_processor":  {"i686", "x86_64"},
 	"mac_processor":    {"Intel", "PPC", "U; Intel", "U; PPC"},
-	"windows_platform": {"Windows NT 10.0", "Windows NT 6.3", "Windows NT 6.2", "Windows NT 6.1", "Windows NT 6.0"},
+	"windows_platform": {"Windows NT 10.0", "Windows NT 6.3", "Windows NT 6.2", "Windows NT 6.1"},
 	"windows_version":  {"", "; WOW64", "; Win64; x64"},
 }
